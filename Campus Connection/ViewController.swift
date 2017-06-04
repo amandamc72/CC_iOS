abd6d262619251813cdbd5fecd9e6894c8be453f
amandamc72 <amccart8@emich.edu>
2017-06-03 17:40:55 -0700

    Login ui

--- Campus Connection/ViewController.swift
+++ Campus Connection/ViewController.swift
@@ -2,24 +2,52 @@
 //  ViewController.swift
 //  Campus Connection
 //
-//  Created by Amanda on 5/20/17.
 //
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    //MARK: Properties
+    @IBOutlet weak var emailLoginInput: UITextField!
+    @IBOutlet weak var passwordLoginInput: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        emailLoginInput.delegate = self;
+        passwordLoginInput.delegate = self;
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField == passwordLoginInput {
+            attemptLogin()
+        }
+    }
+    
+    //MARK: Actions
+    @IBAction func signupButton(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func loginButton(_ sender: UIButton) {
+        attemptLogin()
+    }
+    
+    func attemptLogin() {
+        
+    }
 }
 
